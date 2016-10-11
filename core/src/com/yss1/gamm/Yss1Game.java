c27e4c6e8de7e5e745f444ca872d8cc990581a65
yss1W <yss1@yandex.ru>
2016-10-11 10:10:57 +0800

    Допилил

--- core/src/com/yss1/gamm/Yss1Game.java
+++ core/src/com/yss1/gamm/Yss1Game.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Yss1Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img1;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img1 = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,13 +21,13 @@ public class Yss1Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img1, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
 	}
 }
