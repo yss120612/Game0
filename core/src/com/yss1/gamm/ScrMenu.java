68ed6949f489d15ed12b8bdadc64db0091f4e0e9
yss1W <yss1@yandex.ru>
2016-10-11 10:19:39 +0800

    Сделал экран

---
+++ core/src/com/yss1/gamm/ScrMenu.java
@@ -0,0 +1,57 @@
+package com.yss1.gamm;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by a0804 on 11.10.2016.
+ */
+
+public class ScrMenu implements Screen {
+    private Yss1Game game;
+
+    public ScrMenu(Game game)
+    {
+        this.game=(Yss1Game)game;
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(game.img1, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/yss1/gamm/Yss1Game.java
+++ core/src/com/yss1/gamm/Yss1Game.java
@@ -1,29 +1,26 @@
 package com.yss1.gamm;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Yss1Game extends ApplicationAdapter {
+public class Yss1Game extends Game {
 	SpriteBatch batch;
 	Texture img1;
-	
+	Screen scrMenu,scrGame;
 	@Override
 	public void create () {
+		scrMenu=new ScrMenu(this);
 		batch = new SpriteBatch();
 		img1 = new Texture("badlogic.jpg");
+		setScreen(scrMenu);
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img1, 0, 0);
-		batch.end();
-	}
+
 	
 	@Override
 	public void dispose () {
