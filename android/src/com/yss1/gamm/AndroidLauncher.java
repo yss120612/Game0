1aacfd8e64dfa7870b32cee518a9193c6eeb4890
yss1W <yss1@yandex.ru>
2016-10-11 10:02:17 +0800

    Initial commit

---
+++ android/src/com/yss1/gamm/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yss1.gamm;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yss1.gamm.Yss1Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Yss1Game(), config);
+	}
+}
---
+++ desktop/src/com/yss1/gamm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yss1.gamm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yss1.gamm.Yss1Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Yss1Game(), config);
+	}
+}
